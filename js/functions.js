d2f73557b6cd94a87ab4e1382f886d57760a0a73
Wilber Zada Rosendi <wil63r@gmail.com>
2016-09-09 14:13:37 -0300

    Arreglada y completada la página Tienda

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('.fancybox').fancybox({
+    openEffect  : 'elastic',
+    closeBtn: false,
+    arrows: true,
+    helpers : {
+      title : {type : 'inside'},
+      overlay : {css : {'background' : 'rgba(0,0,0,0.7)'} }
+    }
+  });
+});
+(function () {
+  'use strict';
+  var feed = new Instafeed({
+    get: 'user',
+    sortBy: 'random',
+    limit: '12',
+    userId: '1574668298',
+    accessToken: '1574668298.f6097ab.4c9f0dee55a547e3a7aedbefba42dd01',
+    clientId: 'f6097aba6ca44311a57712308fae63c3',
+    resolution: 'standard_resolution',
+    template: '<a class="fancybox" href="{{image}}"><img class="gallery-thumbs" src="{{image}}" /></a>'
+  });
+  feed.run();
+} ());
