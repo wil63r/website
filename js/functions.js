a274c3cc982b7f48f71797ba5c3abc895c882ac0
Wilber Zada Rosendi <wil63r@gmail.com>
2018-03-08 10:18:01 -0500

    Actualización general del Sitio

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,19 @@
-$(function() {
-  $('.fancybox').fancybox({
-    openEffect  : 'elastic',
-    closeBtn: false,
-    arrows: true,
-    helpers : {
-      title : {type : 'inside'},
-      overlay : {css : {'background' : 'rgba(0,0,0,0.7)'} }
-    }
-  });
+$('.toggle').on('click', function(){
+	$('#mobile-nav').toggleClass('mostrar');
+	$('.menu').toggleClass('mostrar');
+	$('.top-bar').toggleClass('ocultar');
 });
 (function () {
-  'use strict';
-  var feed = new Instafeed({
-    get: 'user',
-    sortBy: 'random',
-    limit: '18',
-    userId: '1574668298',
-    accessToken: '1574668298.f6097ab.4c9f0dee55a547e3a7aedbefba42dd01',
-    clientId: 'f6097aba6ca44311a57712308fae63c3',
-    resolution: 'standard_resolution',
-    template: '<a class="fancybox" href="{{image}}"><img class="gallery-thumbs" src="{{image}}" /></a>'
-  });
-  feed.run();
+	'use strict';
+	var feed = new Instafeed({
+		get: 'user',
+		sortBy: 'random',
+		limit: '12',
+		userId: '1574668298',
+		accessToken: '1574668298.1677ed0.057a388dd0b3493abca3a5c373799950',
+		clientId: 'f6097aba6ca44311a57712308fae63c3',
+		resolution: 'standard_resolution',
+		template: '<a href="#image{{id}}" class="zoom"> <figure> <img src="{{image}}" class="thumb" /> </figure> </a> <div id="image{{id}}" class="lightbox"> <a href="#_"> <img src="{{image}}" /> </a> <a href="#_"> <span class="exit">X</span> </a> </div>'
+	});
+	feed.run();
 } ());
